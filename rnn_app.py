6093404b701e57162608583185a56ffb095ce43e
Jahnavi Nalajala <146033968+jahnavinalajala@users.noreply.github.com>
2025-03-19 13:05:11 -0500

    rnn_app.py

--- rnn_app.py
+++ rnn_app.py
@@ -7,23 +7,7 @@ from tensorflow.keras.models import load_model
 
 word_index = imdb.get_word_index()
 reverse_word_index = {value:key for key,value in word_index.items()}
-import os
-
-
-# Get current directory
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# Print files in the directory (to debug)
-print("Files in current directory:", os.listdir(BASE_DIR))
-
-# Define model path
-model_path = os.path.join(BASE_DIR, 'model_rnn_imdb.h5')
-
-# Load the model
-if os.path.exists(model_path):
-    model = load_model(model_path)
-else:
-    raise FileNotFoundError(f"Model file not found at {model_path}")
+model = tf.keras.models.load_model('model_rnn_imbd.h5')
 
 def decode_review(encoded_review):
     return ' '.join([reverse_word_index.get(i -3,"?")for i in encoded_review])
